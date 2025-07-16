079b27c3f341a5ce318f7148bb5b1a988da293d1
HARDIK MISRI <97017934+HardikMisri@users.noreply.github.com>
2025-07-16 12:29:37 +0530

    Worked on DataType conversion

---
+++ Basics/DataTypeConversion.js
@@ -0,0 +1,36 @@
+let score = 33;
+
+//console.log(typeof score);
+
+
+score  ="33";
+
+//console.log(typeof(score));
+
+score  ="Hardik";
+
+console.log(typeof(score));
+
+
+let changescore  =Number(score);
+
+console.log(typeof(changescore));
+
+console.log(changescore);
+
+//"33" -> 33
+//"33abc" -> NaN (not a number)
+//true -> 1
+
+
+let isLoggedIn = "har";
+let booleanisLoggedIn = Boolean(isLoggedIn);
+
+
+console.log(booleanisLoggedIn);
+console.log(typeof(booleanisLoggedIn))
+
+//"har" -> true;
+//"" -> false;
+// 1 -> true;
+//0 -> false;
