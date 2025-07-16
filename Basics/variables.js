5668685c5feea7bc68de7760363e37149a12a112
HARDIK MISRI <97017934+HardikMisri@users.noreply.github.com>
2025-07-16 11:55:57 +0530

    Learned how variables work in JavaScript

---
+++ Basics/variables.js
@@ -0,0 +1,24 @@
+const accountId = 11445
+
+let accountEmail = "hardikmishri34@gmail.com"
+
+var accountPassword = "12345"
+
+accountCity ="Mumbai"
+
+let accountState
+
+
+//accountId = 2
+
+
+/*
+Prefer not to use 'var'
+because it has function scope and can lead to unexpected behavior.
+*/
+accountEmail = "hc@gmail.com"
+accountPassword= "2121"
+accountCity  ="Delhi"
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountState]);
+
