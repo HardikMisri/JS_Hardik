d7cdc46347a04c68418a6cc688d2074120f412e5
HARDIK MISRI <97017934+HardikMisri@users.noreply.github.com>
2025-07-16 12:13:29 +0530

    Learned  Data_Types of JavaScript

---
+++ Basics/Data_Types.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// alert(3+3); //we are using node js not browser 
+
+let age =14
+let name ="hardik";
+
+console.log(typeof age);
+console.log(typeof null); //type is object
+
+//Data type in js 
+//Symbol : Used for unique
+//bigint
+//undefined
+//null
+//boolean
+//object
+//string
+//number
+
+
+console.table([typeof name , typeof age, typeof undefined ]);
