2f04d6ae9a22d62aca1cbd91f3f2ad50bfd3b82a
HARDIK MISRI <97017934+HardikMisri@users.noreply.github.com>
2025-07-17 12:38:04 +0530

    Math & Number

---
+++ Basics/number.js
@@ -0,0 +1,36 @@
+const score  = 400
+// console.log(score);
+const newscore = new Number(400); //return object 
+// console.log(newscore);
+
+// console.log(newscore.toString());
+
+// console.log(newscore.toFixed(2));
+
+const othernum =2345.8906
+
+// console.log(othernum.toPrecision(4));
+
+const hundereds = 1000000
+// console.log(hundereds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++Math+++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+// console.log(Math.floor(4.9 ));
+
+// console.log(Math.min(4,5,2,1,6));
+
+console.log(Math.random()); //0-1 values between
+
+console.log((Math.random()*10)+1);
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max =20;
+
+console.log(Math.floor(Math.random()* (max-min+1)) + min);
+
